33c40646bbabf25e751e2c5592cb4fe0223fbeb2
Kevin Smith <Kevin.Smith@microsoft.com>
2019-04-13 17:33:10 -0700

    Fix error callback tests

--- test/callback-error.js
+++ test/callback-error.js
@@ -10,6 +10,7 @@ describe('callback-error', () => {
       observer.next = next;
       observer.error = error;
       observer.complete = complete;
+      return () => observer.cleanupCalled = true;
     }).listen(...args);
 
     return observer;
@@ -22,121 +23,95 @@ describe('callback-error', () => {
     assert.deepEqual(args, [1]);
   });
 
-  it('does not return a value', () => {
-    let observer = getObserver({ error() { return 1 } });
-    assert.equal(observer.error(), undefined);
+  it('returns a value', () => {
+    let observer = getObserver(null, () => 1);
+    assert.equal(observer.error(), 1);
   });
 
-  it('does not throw when the subscription is complete', () => {
-    let observer = getObserver({ error() {} });
+  it('callback is not called when the subscription is complete', () => {
+    let called = false;
+    let observer = getObserver(null, () => called = true);
     observer.complete();
-    observer.error('error');
+    observer.error();
+    assert.equal(called, false);
   });
 
-  it('does not throw when the subscription is cancelled', () => {
-    let observer;
-    let subscription = new EventStream(x => { observer = x }).subscribe({
-      error() {},
-    });
-    subscription.unsubscribe();
-    observer.error(1);
-    assert.ok(!hostError);
+  it('callback is not called when the subscription is cancelled', () => {
+    let called = false;
+    let observer = getObserver(null, () => called = true);
+    observer.cancel();
+    observer.error();
+    assert.equal(called, false);
   });
 
-  it('queues if the subscription is not initialized', async () => {
-    let error;
-    new EventStream(x => { x.error({}) }).subscribe({
-      error(err) { error = err },
-    });
-    assert.equal(error, undefined);
-    await null;
-    assert.ok(error);
+  it('throws when there is no handler', () => {
+    let observer = getObserver();
+    assert.throws(() => observer.error());
   });
 
-  it('queues if the observer is running', async () => {
-    let observer;
-    let error;
-    new EventStream(x => { observer = x }).subscribe({
-      next() { observer.error({}) },
-      error(e) { error = e },
-    });
-    observer.next();
-    assert.ok(!error);
-    await null;
-    assert.ok(error);
+  it('does not throw when there is no handler and subscription is done', () => {
+    let observer = getObserver();
+    observer.complete();
+    observer.error();
   });
 
-  it('closes the subscription before invoking inner observer', () => {
-    let closed;
-    let observer = getObserver({
-      error() { closed = observer.closed },
-    });
-    observer.error(1);
-    assert.equal(closed, true);
+  it('does not throw when there is no handler and subscription is cancelled', () => {
+    let observer = getObserver();
+    observer.cancel();
+    observer.error();
   });
 
-  it('reports an error if "error" is not a method', () => {
-    let observer = getObserver({ error: 1 });
-    observer.error(1);
-    assert.ok(hostError);
+  it('closes the subscription before invoking callback', () => {
+    let observer = getObserver(null, () => {});
+    observer.error();
+    assert.equal(observer.cleanupCalled, true);
   });
 
-  it('reports an error if "error" is undefined', () => {
+  it('throws if callback is not callable', () => {
+    assert.throws(() => getObserver(null, 1).error());
+    assert.throws(() => getObserver(null, {}).error());
+    assert.throws(() => getObserver(null, undefined).error());
+    assert.throws(() => getObserver(null, null).error());
+  });
+
+  it('throws if "error" throws', () => {
     let error = {};
-    let observer = getObserver({ error: undefined });
-    observer.error(error);
-    assert.equal(hostError, error);
-  });
-
-  it('reports an error if "error" is null', () => {
-    let error = {};
-    let observer = getObserver({ error: null });
-    observer.error(error);
-    assert.equal(hostError, error);
-  });
-
-  it('reports error if "error" throws', () => {
-    let error = {};
-    let observer = getObserver({ error() { throw error } });
-    observer.error(1);
-    assert.equal(hostError, error);
+    let observer = getObserver(null, () => { throw error });
+    assert.throws(() => observer.error());
   });
 
   it('calls the cleanup method after "error"', () => {
     let calls = [];
-    let observer;
-    new EventStream(x => {
-      observer = x;
+    let observer = {};
+    new EventStream((next, error) => {
+      observer.error = error;
       return () => { calls.push('cleanup') };
-    }).subscribe({
-      error() { calls.push('error') },
-    });
+    }).listen(null, () => calls.push('error'));
     observer.error();
     assert.deepEqual(calls, ['error', 'cleanup']);
   });
 
-  it('calls the cleanup method if there is no "error"', () => {
+  it('calls the cleanup method if there is no callback', () => {
     let calls = [];
-    let observer;
-    new EventStream(x => {
-      observer = x;
+    let observer = {};
+    new EventStream((next, error) => {
+      observer.error = error;
       return () => { calls.push('cleanup') };
-    }).subscribe({});
+    }).listen();
     try {
       observer.error();
     } catch (err) {}
     assert.deepEqual(calls, ['cleanup']);
   });
 
-  it('reports error if the cleanup function throws', () => {
+  it('throws if the cleanup function throws', () => {
     let error = {};
-    let observer;
-    new EventStream(x => {
-      observer = x;
+    let observer = error;
+    new EventStream((next, error) => {
+      observer.error = error;
       return () => { throw error };
-    }).subscribe();
-    observer.error(1);
-    assert.equal(hostError, error);
+    }).listen();
+    assert.throws(() => observer.error());
   });
 
 });
